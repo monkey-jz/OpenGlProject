4cb21742cacb14da78bea9691f63683ba4d3f98a
JerryZhu <773749686@qq.com>
2022-11-02 17:22:11 +0800

    first commit

---
+++ app/src/main/java/com/jerry/openglproject/OpenGLES20Activity.java
@@ -0,0 +1,23 @@
+package com.jerry.openglproject;
+
+import android.app.Activity;
+import android.os.Bundle;
+import androidx.annotation.Nullable;
+
+/**
+ * @author: JerryZhu
+ * @datetime: 2022/11/2
+ */
+public class OpenGLES20Activity extends Activity {
+    public static final String TAG = "OpenGLES20Activity";
+    private MyGLSurfaceView gLView;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Create a GLSurfaceView instance and set it
+        // as the ContentView for this Activity.
+        gLView = new MyGLSurfaceView(this);
+        setContentView(gLView);
+    }
+}
